ac640a94cb26ca401aeb602ab1012e43624a49f1
Marine Teyssere <marine.teyssere@gmail.com>
2022-04-23 11:50:27 +0200

    form data

--- modal.js
+++ modal.js
@@ -26,51 +26,99 @@ function closeModal() {
 }
 
 //Formulaire 
-function validate(form) { 
-  let firstNameValid = checkCondition(form["first"].value) && checkCondition(form["first"].value.length >= 2);
-  firstNameValid ? 
-    hideErrorMessage('error-firstName', form["first"]) : 
-    getErrorMessage('error-firstName', "Veuillez entrer 2 caractères ou plus pour le champ du prénom.", form["first"]);
+
+
+function validateFirstName() {
+
+  let vrai = 'firstNameValid'
+  let faux = formData[data-error-visible]="true" ='Veuillez entrer 2 caractères ou plus pour le champ du prénom'
+
+  if (first = length >= 2){
+      return vrai ;
+  }else{
+     return faux ; 
+  }
   
-  let lastNameValid = checkCondition(form["last"].value) && checkCondition(form["last"].value.length >= 2);
-  lastNameValid ?  
-    hideErrorMessage('error-lastName', form["last"]) : 
-    getErrorMessage('error-lastName', "Veuillez entrer 2 caractères ou plus pour le champ du nom.", form["last"]); 
-  
-
-  let emailValid = checkCondition(form["email"].value) && checkCondition(/[A-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(form["email"].value));
-  emailValid ? 
-    hideErrorMessage('error-email', form["email"]) : 
-    getErrorMessage('error-email', "Veuillez entrer une addresse mail valide.", form["email"]);
-
-  let birthdateValid = checkCondition(form["birthdate"].value) && checkCondition(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/.test(form["birthdate"].value));
-  birthdateValid ? 
-    hideErrorMessage('error-birthdate', form["birthdate"]) : 
-    getErrorMessage('error-birthdate', "Veuillez entrer une date de naissance.", form["birthdate"]);
-
-  //isNaN return false if is a number, true if not
-  let qteTournamentValid = checkCondition(form["quantity"].value) && checkCondition(/^[0-9]+$/.test(form["quantity"].value));
-  qteTournamentValid ? 
-    hideErrorMessage('error-tournament', form["quantity"]) : 
-    getErrorMessage('error-tournament', "Veuillez entrer une valeur numérique.", form["quantity"]);
-
-  let locationValid = checkCondition(form.location.value);
-  locationValid ?  hideErrorMessage('error-location') : getErrorMessage('error-location', "Veuillez sélectionner une ville.");
-
-  let termsValid = checkCondition(form.terms.checked);
-  termsValid ? hideErrorMessage('error-terms') : getErrorMessage('error-terms', "Veuillez indiquer que vous acceptez les conditions générales.");
-
-  // Check the confirmation form, show a confirmation message
-  if(
-      firstNameValid 
-      && lastNameValid 
-      && emailValid
-      && birthdateValid 
-      && qteTournamentValid
-      && locationValid
-      && termsValid
-    ) {
-      document.querySelector(".modal-body").style.display = "none";
-      document.querySelector(".formConfirmation").style.display = "block";
-    }
 }
+
+function validateLasteName() {
+
+  let vrai = 'lastNameValid'
+  let faux = 'Veuillez entrer 2 caractères ou plus pour le champ du nom'
+
+  if (last = length >= 2){
+      return vrai ;
+  }else{
+     return faux;
+     [data-error-visible="true"]
+  }
+  
+}
+
+function validateEmail() {
+
+  let vrai = 'emailValid'
+  let faux = 'Veuillez entrer une addresse mail valide.'
+
+  if (email = (/[A-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(form["email"].value)) {
+      return vrai ;
+  }else{
+     return faux
+  }
+
+
+}
+function validatebirthdate() {
+
+  let vrai = 'birthdateValid'
+  let faux = 'Veuillez entrer une date de naissance.'
+
+  if (birthdate = form["birthdate"]){
+      return vrai ;
+  }else{
+     return faux
+  }
+  
+}
+
+function validateTournament() {
+
+  let vrai = 'tournamentValid'
+  let faux = 'Veuillez entrer une valeur numérique.'
+
+  if (form["quantity"].value){
+      return vrai ;
+  }else{
+     return faux
+  }
+  
+}
+
+function validatelocation() {
+
+  let vrai = 'locationValid'
+  let faux = 'Veuillez sélectionner une ville.'
+
+  if (form.location.value){
+      return vrai ;
+  }else{
+     return faux
+  }
+  
+
+
+}
+function validateterms() {
+
+  let vrai = 'termsValid'
+  let faux = 'Veuillez indiquer que vous acceptez les conditions générales.'
+
+  if (form.terms.checked){
+      return vrai ;
+  }else{
+     return faux
+  }
+
+
+}
+
