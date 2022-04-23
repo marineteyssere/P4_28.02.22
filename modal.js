5604f3466db966aee80a7de55934e77697a80470
Marine Teyssere <marine.teyssere@gmail.com>
2022-04-23 11:57:24 +0200

    test 1 fonction

--- modal.js
+++ modal.js
@@ -31,94 +31,12 @@ function closeModal() {
 function validateFirstName() {
 
   let vrai = 'firstNameValid'
-  let faux = formData[data-error-visible]="true" ='Veuillez entrer 2 caractères ou plus pour le champ du prénom'
-
+  formData[data-error-visible]="true" 
   if (first = length >= 2){
       return vrai ;
   }else{
-     return faux ; 
+     return formData[data-error-visible] +='Veuillez entrer 2 caractères ou plus pour le champ du prénom'; 
   }
   
-}
-
-function validateLasteName() {
-
-  let vrai = 'lastNameValid'
-  let faux = 'Veuillez entrer 2 caractères ou plus pour le champ du nom'
-
-  if (last = length >= 2){
-      return vrai ;
-  }else{
-     return faux;
-     [data-error-visible="true"]
-  }
-  
-}
-
-function validateEmail() {
-
-  let vrai = 'emailValid'
-  let faux = 'Veuillez entrer une addresse mail valide.'
-
-  if (email = (/[A-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(form["email"].value)) {
-      return vrai ;
-  }else{
-     return faux
-  }
-
-
-}
-function validatebirthdate() {
-
-  let vrai = 'birthdateValid'
-  let faux = 'Veuillez entrer une date de naissance.'
-
-  if (birthdate = form["birthdate"]){
-      return vrai ;
-  }else{
-     return faux
-  }
-  
-}
-
-function validateTournament() {
-
-  let vrai = 'tournamentValid'
-  let faux = 'Veuillez entrer une valeur numérique.'
-
-  if (form["quantity"].value){
-      return vrai ;
-  }else{
-     return faux
-  }
-  
-}
-
-function validatelocation() {
-
-  let vrai = 'locationValid'
-  let faux = 'Veuillez sélectionner une ville.'
-
-  if (form.location.value){
-      return vrai ;
-  }else{
-     return faux
-  }
-  
-
-
-}
-function validateterms() {
-
-  let vrai = 'termsValid'
-  let faux = 'Veuillez indiquer que vous acceptez les conditions générales.'
-
-  if (form.terms.checked){
-      return vrai ;
-  }else{
-     return faux
-  }
-
-
 }
 
